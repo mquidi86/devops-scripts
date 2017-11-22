bc03a87f6c67cbbd1983a871b9daad6048d3f7be
Mariano Quidiello <mariano@jostle.me>
2017-11-22 09:51:31 -0800

    Changed aws-stack-list-all-regions.sh to look specifically for a stack if passed as argument

--- aws-stack-list-all-regions.sh
+++ aws-stack-list-all-regions.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+REGIONS_FILE=`dirname $0`/aws-regions.lst
+stack_name=$1
 printf "%s\n" "Listing CloudFormation Stacks for all regions" 
 
-#aws rds describe-db-instances --region "${rds_region}" "${rds_name:+--db-instance-identifier}" ${rds_name} --query "DBInstances[*].[DBInstanceIdentifier,DBInstanceStatus,DBSubnetGroup.VpcId,MasterUsername,DBName]" --output table
 IFS="|"
 while read stack_region region_name 
 do
-	aws cloudformation describe-stacks --region "${stack_region}" "${stack_name:+--stack-name}" ${stack_name} --query "Stacks[*].[StackName,StackStatus,CreationTime,StackStatusReason,DisableRollback]" --output table | sed "s?^?${stack_region} | ?" 
-done < `dirname $0`/aws-regions.lst
+	aws cloudformation describe-stacks --region "${stack_region}" ${stack_name:+--stack-name} ${stack_name} \
+	--output table \
+	--query "Stacks[*].[to_string(\`${stack_region} \`),StackName,StackStatus,CreationTime,StackStatusReason,DisableRollback]" 2>/dev/null
+done < ${REGIONS_FILE}
