6152f908cdd3e7c6d02e84afb8aecb8ad4f40fc2
Mariano Quidiello <mariano@jostle.me>
2017-11-29 11:46:36 -0800

    Fixed aws-autoscaling-groups-list.sh script

--- aws-autoscaling-groups-list.sh
+++ aws-autoscaling-groups-list.sh
@@ -5,5 +5,5 @@ export asg_name="$2"
 printf "%s\n" "Listing AutoScaling Groups" 
 
 aws autoscaling describe-auto-scaling-groups --region "${aws_region}" ${asg_name:+--auto-scaling-group-names} ${asg_name} \
-#	--output table \
-	#--query "AutoScalingGroups[*].[AutoScalingGroupName,Status,LaunchConfigurationName,VPCZoneIdentifier,length(Instances)]"
+	--output table \
+	--query "AutoScalingGroups[*].[AutoScalingGroupName,Status,LaunchConfigurationName,VPCZoneIdentifier,length(Instances)]"
