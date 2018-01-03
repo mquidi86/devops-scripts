83ec5e9a4e66bf9db7a49149ff3c4ef00add8a08
Mariano Quidiello <mariano@jostle.me>
2018-01-03 10:05:36 -0800

    changed region for Advisor checks

--- aws-advisor-check-ids.sh
+++ aws-advisor-check-ids.sh
@@ -1,1 +1,1 @@
-aws support describe-trusted-advisor-checks --language en --query "checks[?category=='cost_optimizing'].[id,name]" --output text | sort -k2
+aws support describe-trusted-advisor-checks --region us-east-1 --language en --query "checks[?category=='cost_optimizing'].[id,name]" --output text | sort -k2
