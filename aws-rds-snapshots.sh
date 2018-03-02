9989253425dc1394b44c1c14ada6bd78f00c9f11
Mariano Quidiello <mariano@jostle.me>
2018-03-02 11:36:45 -0800

    Modified aws-rds-snapshots.sh so that it present the snapshots ordered by Creation Time

--- aws-rds-snapshots.sh
+++ aws-rds-snapshots.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export rds_region="$1"
+export aws_region="$1"
 export rds_name="$2"
 
 printf "%s\n" "Listing RDS Snapshots ${rds_name:+for} ${rds_name}" 
 
-aws rds describe-db-snapshots --region "${rds_region}" "${rds_name:+--db-instance-identifier}" ${rds_name} \
---query "DBSnapshots[*].[DBSnapshotIdentifier,Status,SnapshotCreateTime,PercentProgress,SnapshotType,StorageType,DBInstanceIdentifier,VpcId]" --output table
+aws rds describe-db-snapshots --region "${aws_region}" "${rds_name:+--db-instance-identifier}" ${rds_name} \
+--query "sort_by(DBSnapshots,&SnapshotCreateTime)[].[DBSnapshotIdentifier,Status,SnapshotCreateTime,PercentProgress,SnapshotType,StorageType,DBInstanceIdentifier,VpcId]" --output table
